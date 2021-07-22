7c7f666f2db790189c024d62c767382f85964e50
jhonny <jhonny@pop-os.localdomain>
2021-07-22 15:25:11 -0500

    Primer commit

--- server.js
+++ server.js
@@ -1,24 +1,69 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const dns = require("dns");
+const urlParser = require("url");
 const app = express();
+
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+console.log(mongoose.connection.readyState);
+
+const urlSchema = new Schema({
+  url: String, // String is shorthand for {type: String}
+});
+
+const Url = mongoose.model("Url", urlSchema);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use("/public", express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.post("/api/shorturl", function (req, res) {
+  console.log(req.body);
+  const bodyUrl = req.body.url;
+  const preUrl = dns.lookup(
+    urlParser.parse(bodyUrl).hostname,
+    (err, address) => {
+      console.log(err);
+      if (!address) return res.json({ error: "Invalid URL" });
+
+      const url = new Url({ url: bodyUrl });
+      url.save((err, data) => {
+        if (err) return console.error(err);
+        res.json(data);
+      });
+    }
+  );
+});
+
+app.get("/api/shorturl/:id", function (req, res) {
+  const id = req.params.id;
+  Url.findById(id, (err, data) => {
+    if (!data) return res.json({ error: "Invalid URL" });
+    res.redirect(data.url);
+  });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
